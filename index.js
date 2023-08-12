f2175131eb2166b90f0d4407e6417ec67464730c
John Clarence Palma <johnclarencepalma7@gmail.com>
2023-08-12 16:10:00 +0800

    add file

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
+// import express from "express";
+// const app = express();
+// const server = 7000;
+
+// app.get("/", (req, res) => {
+// 	res.send("<h1>Hello!<h1/>");
+// });
+
+// app.listen(server, () => {
+// 	console.log(`port is running`);
+// });
+
 import express from "express";
 const app = express();
-const server = 7000;
 
 app.get("/", (req, res) => {
-	res.send("<h1>Hello!<h1/>");
+	res.send("Hello rence page");
 });
 
-app.listen(server, () => {
-	console.log(`port is running`);
+app.get("/about", (req, res) => {
+	res.send("Just doing good like always");
 });
+
+app.get("/about/home", (req, res) => {
+	res.send("home");
+});
+
+app.listen(5000, () => {
+	console.log("App running succesfully");
+});
+
+//command netstat -ano | findstr "LISTENING"
