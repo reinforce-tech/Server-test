03f2addc29c6780bf634a187cff4035f9775de37
John Clarence Palma <johnclarencepalma7@gmail.com>
2023-08-12 16:14:53 +0800

    file update

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// import express from "express";
-// const app = express();
-// const server = 7000;
-
-// app.get("/", (req, res) => {
-// 	res.send("<h1>Hello!<h1/>");
-// });
-
-// app.listen(server, () => {
-// 	console.log(`port is running`);
-// });
-
 import express from "express";
 const app = express();
 
