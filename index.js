2c0f4b1c8eeb349f767966bff6a763ceb8da3cbf
John Clarence Palma <johnclarencepalma7@gmail.com>
2023-08-16 21:51:33 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send("Hello rence page");
-});
-
-app.get("/about", (req, res) => {
-	res.send(" about me nothing much just like to type things");
-});
-
-app.get("/home", (req, res) => {
-	res.send("You are in home ");
-});
-
-app.get("/about/home", (req, res) => {
-	res.send("You are in home");
-});
-
-app.get("/home/about", (req, res) => {
-	res.send("about me nothing much just like to type things ");
-});
-
-app.listen(5000, () => {
-	console.log("App running succesfully");
-});
-
 //command netstat -ano | findstr "LISTENING"
+
+app.listen(3000, () => {
+	console.log("running on port 3000 ");
+	//server port
+});
+
+app.get("/", (req, res) => {
+	res.send("<h1>Home Page</h1>");
+});
+
+app.post("/register", (req, res) => {
+	//Do something with the data9
+	res.sendStatus(201);
+});
+
+app.put("/user/rence", (req, res) => {
+	res.sendStatus(200);
+});
+
+app.patch("/user/rence", (req, res) => {
+	res.sendStatus(200);
+});
+
+app.delete("/user/rence", (req, res) => {
+	//Deleting
+	res.sendStatus(200);
+});
